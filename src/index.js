5cc5a4b02d88717205bace836334a4a2ba7a35d0
liptonzuma <solomonboateng231@gmail.com>
2021-10-06 21:25:49 +0000

    slight changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-import React,{useEffect} from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.css'
 import  '../src/styles/styles.css'
 import Navbar from './components/Navbar'
-import Header from './components/Header'
-import gsap from 'gsap'
 function App(){
-  useEffect(()=>{
-    gsap.from('.main',{
-    y:'-100%',
-    delay:1
-  })
-  
-},[])
+ 
+
   return(
     <div className="main">
       <Navbar />
-      <Header />
     </div>
   )
 }
